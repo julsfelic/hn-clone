fb795f77d966974c6d14c5ba3d6127c0c6066891
Julian Feliciano <julsfelic@gmail.com>
2020-08-02 11:30:19 -0500

    Flesh out top posts page

--- src/api/hackerNews.js
+++ src/api/hackerNews.js
@@ -1,4 +1,5 @@
 const topStoriesUrl = "https://hacker-news.firebaseio.com/v0/topstories.json";
+const newStoriesUrl = "https://hacker-news.firebaseio.com/v0/newstories.json";
 
 function itemUrl(id) {
   return `https://hacker-news.firebaseio.com/v0/item/${id}.json`;
@@ -37,6 +38,15 @@ export async function fetchUser(id) {
 export async function fetchTopPostsIds() {
   try {
     const response = await fetch(topStoriesUrl);
+    return await response.json();
+  } catch (e) {
+    throw new Error(`Oh no!!!!!. We got the error: ${e}`);
+  }
+}
+
+export async function fetchNewPostsIds() {
+  try {
+    const response = await fetch(newStoriesUrl);
     return await response.json();
   } catch (e) {
     throw new Error(`Oh no!!!!!. We got the error: ${e}`);
--- src/components/NewPosts.js
+++ src/components/NewPosts.js
@@ -1,5 +1,32 @@
 import React from "react";
+import ItemList from "./ItemList";
+import Loading from "./Loading";
 
-export default function NewPosts() {
-  return <h1>New Posts</h1>;
+import { fetchNewPostsIds } from "../api/hackerNews";
+
+export default class NewPosts extends React.Component {
+  state = {
+    ids: [],
+    loading: true
+  };
+
+  componentDidMount() {
+    const { ids } = this.state;
+
+    if (!ids.length) {
+      fetchNewPostsIds().then(ids => {
+        this.setState({ ids, loading: false });
+      });
+    }
+  }
+
+  render() {
+    const { ids, loading } = this.state;
+
+    if (loading) {
+      return <Loading />;
+    }
+
+    return <ItemList ids={ids} />;
+  }
 }
